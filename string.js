baea28e6993ae94a648735933eb52c16b8b2e7dc
lalithSrini <lalithsrinivas.t@gmail.com>
2025-02-11 21:44:36 +0530

    add

--- string.js
+++ string.js
@@ -2,9 +2,16 @@ console.log("Hello");
 let x = "     lalith srinivas   "
 let b = `hi check the sentence ${x} bellow`;  // <--- it is known as templet literals
 let a = "HEllo the above one is printed using this:(` `)  ";
-console.log(b);
-console.log(a.toLowerCase());
-let sd = a.toUpperCase();
-console.log(x);
-console.log(x.trim());   // trim the whitespace only starting and ending
-console.log(sd);
+// console.log(b);
+// console.log(a.toLowerCase());
+// let sd = a.toUpperCase();
+// console.log(x);
+// console.log(x.trim());   // trim the whitespace only starting and ending
+// console.log(sd);
+
+
+x = x.trim();  //--> lalith srinivas
+
+console.log(x.slice(2,5)) // starting and ending position
+
+console.log(x.slice(2))  // ending is optional
