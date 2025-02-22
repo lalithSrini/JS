478918bb352ebf7865438ccb5282265c437433e1
lalithSrini <lalithsrinivas.t@gmail.com>
2025-02-22 14:00:40 +0530

    add

---
+++ dom.js
@@ -0,0 +1,36 @@
+// Access Id's
+
+// let a = document.getElementById("ah1");
+// let b = document.getElementById("ah2")
+
+// console.log(a)
+// console.dir(a)
+// console.log(b)
+// console.dir(b)
+
+
+// // Access classes
+
+
+// let c = document.getElementsByClassName("c1")
+
+// console.log(c)
+// console.dir(c)
+
+
+// let d = document.getElementsByTagName("p")
+
+
+// console.log(d)
+
+// console.log(document.getElementsByTagName("h1"))
+
+// console.log(document.querySelector("p"))
+// console.log(document.querySelector("h1"))
+// console.log(document.querySelectorAll("h1"))
+
+
+let c = document.getElementById("ah2")
+
+c.innerText = "TLS"
+c.innerHTML = "<i>Lalith Srinivas</i>"
