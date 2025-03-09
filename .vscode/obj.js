501856a61738ac7a62c0782aff6ba632ee6a7d85
lalithSrini <lalithsrinivas.t@gmail.com>
2025-03-09 10:27:55 +0530

    add

--- .vscode/obj.js
+++ .vscode/obj.js
@@ -1,17 +1,59 @@
-const emp = 
+// // const emp = 
+// // {
+// //     cacl()
+// //     {
+// //         console.log("hello")
+// //     }
+// // }
+
+
+
+// // const lalith = 
+// // {
+// //     salary : 50000,
+// // };
+
+
+// // lalith.__proto__ = emp;
+
+
+// class lalith
+// {
+//     call()
+//     {
+//         console.log("hello world")
+//     }
+// };
+
+// class chi extends lalith {} 
+
+// let k = new chi();
+
+
+// let p = new Promise((resolve,reject) =>
+// {
+//    resolve("hello");
+// //    reject("hell");
+// })
+
+
+
+(function d0o()
 {
-    cacl()
-    {
-        console.log("hello")
-    }
-}
+   setTimeout
+   (() => 
+      {
+      console.log("Step 1 done");
+      }, 
+   1000  //1sec
+   );
 
 
-
-const lalith = 
-{
-    salary : 50000,
-};
-
-
-lalith.__proto__ = emp;
+   setTimeout
+   (() => 
+      {
+      console.log("Step 2 done");
+      }, 
+   2000  //1sec
+   );
+})();
